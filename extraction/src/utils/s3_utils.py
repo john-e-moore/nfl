b4d44ad5cc0636ad4df67c6ee984f3670b6eac0b
John Moore <jmoore87jr@gmail.com>
2023-07-09 16:11:45 -0400

    S3 utils finished

--- extraction/src/utils/s3_utils.py
+++ extraction/src/utils/s3_utils.py
@@ -1,17 +1,47 @@
 # Standard
 import json
-import typing
+from typing import Optional
 # External
 import boto3
+# Internal
+from utils.logger import get_logger
 
-def check_file_exists(s3, bucket, key):
-    pass
+logger = get_logger(__name__)
 
-def fetch_file_size(s3, bucket, key):
-    pass
+def check_file_exists(s3: boto3.resources.factory.s3.ServiceResource, bucket: str, key: str) -> bool:
+    """Checks if an S3 object exists. Returns true/false."""
+    try:
+        s3.head_object(Bucket=bucket, Key=key)
+        return True
+    except:
+        return False
 
-def fetch_last_update_timestamp(s3, bucket, key, job):
-    pass
+def fetch_file_size(s3: boto3.resources.factory.s3.ServiceResource, bucket: str, key: str) -> Optional[int]:
+    """Reads the file size metadata for an S3 object
+    and returns it as an integer (number of bytes)."""
+    try:
+        file_info = s3.head_object(Bucket=bucket, Key=key)
+        return file_info['ContentLength']
+    except Exception as e:
+        logger.info(f"Error occurred while fetching file size: {e}")
+        return None
 
-def write_last_update_timestamp(s3, bucket, key, job):
-    pass
+def fetch_last_update_timestamp(s3: boto3.resources.factory.s3.ServiceResource, bucket: str, key: str, job_name: str) -> Optional[str]:
+    """Reads the last updated metadata for an S3 object
+    and returns it as a string."""
+    try:
+        file_info = s3.head_object(Bucket=bucket, Key=key)
+        return str(file_info['LastModified'])
+    except Exception as e:
+        logger.info(f"Error occurred while fetching last update timestamp: {e}")
+        return None
+
+def write_last_update_timestamp(s3: boto3.resources.factory.s3.ServiceResource, bucket: str, key: str, job_name: str) -> None:
+    """Writes a .txt file to S3 with the timestamp
+    that another file in that folder was last updated (job_name)."""
+    try:
+        timestamp = fetch_last_update_timestamp(s3, bucket, key, job_name)
+        if timestamp:
+            s3.put_object(Body=timestamp, Bucket=bucket, Key=key)
+    except Exception as e:
+        logger.info(f"Error occurred while writing last update timestamp: {e}")
