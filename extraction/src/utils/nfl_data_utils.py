7055eab241a38cc05f2773de04401fd17dcec86d
John Moore <jmoore87jr@gmail.com>
2023-07-09 16:32:11 -0400

    allowing for csv, json, parquet choice. need to add argument parser to main

--- extraction/src/utils/nfl_data_utils.py
+++ extraction/src/utils/nfl_data_utils.py
@@ -1,13 +1,34 @@
 # Standard
 from typing import List
+from io import BytesIO, StringIO
 # External
 import pandas as pd
 import nfl_data_py as nfl
+import pyarrow.parquet as pq
+# Internal
+from utils.logger import get_logger
 
-def fetch_pbp(years: List[int]) -> None:
+logger = get_logger(__name__)
+
+def fetch_pbp(years: List[int], format='json') -> None:
     """Gets play-by-play data from nfl_data_py and converts to JSON."""
     
     df = nfl.import_pbp_data(years=years, downcast=True, cache=False, alt_path=None)
-    json_data = df.to_json(orient='records')
 
-    return json_data
+    if format.lower() == 'json':
+        out_buffer = StringIO()
+        return df.to_json(path_or_buff=out_buffer, orient='records', index=False)
+    
+    elif format.lower() == 'csv':
+        out_buffer = StringIO()
+        return df.to_csv(out_buffer, index=False)
+
+    elif format.lower() == 'parquet':
+        out_buffer = BytesIO()
+        return df.to_parquet(out_buffer, index=False)
+    
+    else:
+        logger.info("Valid formats are json, csv, parquet.")
+        return None
+
+    
