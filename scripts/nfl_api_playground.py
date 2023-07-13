74162399f1f9b60d8b8032849ce5f3a5384f204a
John Moore <jmoore87jr@gmail.com>
2023-07-13 17:43:46 -0400

    typo

--- scripts/nfl_api_playground.py
+++ scripts/nfl_api_playground.py
@@ -1,5 +1,6 @@
 import nfl_data_py as nfl
 import pandas as pd 
+import time
 
 pbp_cols = nfl.see_pbp_cols().to_list()
 
@@ -7,6 +8,8 @@ with open('/home/jmlaptop/nfl/pbp_cols.txt', 'w') as f:
     for i,col in enumerate(pbp_cols):
         f.write(f'({i}) {col}\n')
 
-df = nfl.import_pbp_data(2022).head()
+time.sleep(5)
+
+df = nfl.import_pbp_data([2022]).head()
 
 df.to_csv('/home/jmlaptop/nfl/pbp_sample.csv')
